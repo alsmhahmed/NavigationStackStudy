c25555a1d815eaa7a5c35dd65c295cbd228f6bb2
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-03-09 14:38:36 +0200

    test to develop

---
+++ NavigationStackStudy/test2.swift
@@ -0,0 +1,18 @@
+//
+//  test2.swift
+//  NavigationStackStudy
+//
+//  Created by alsmh ahmed on 09/03/2025.
+//
+
+import SwiftUI
+
+struct test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test2()
+}
