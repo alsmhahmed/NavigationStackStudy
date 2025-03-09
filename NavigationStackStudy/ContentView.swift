55ca339d71c30377f6aba30babe17c2097da17d8
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-03-09 13:07:17 +0200

    finsh the NavigationStack example

--- NavigationStackStudy/ContentView.swift
+++ NavigationStackStudy/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
                                   .init(name: "PlayStation", imageName: "playstation.logo", color: .indigo),
                                   .init(name: "PC", imageName: "pc", color: .pink),
                                   .init(name: "Mobile", imageName: "iphone", color: .mint)]
+    
+    var games: [Game] = [ .init(name: "Minecraft", rating: "99") ,
+                          .init(name: "God of War", rating: "98") ,
+                          .init(name: "fortnite", rating: "95"),
+                          .init(name: "PES", rating: "100")
+                          
+    ]
     var body: some View {
         NavigationStack {
             List{
@@ -21,6 +28,13 @@ struct ContentView: View {
                         NavigationLink(value: platform) {
                             Label(platform.name, systemImage: platform.imageName)
                                 .foregroundStyle(platform.color)
+                        }
+                    }
+                }
+                Section("Games") {
+                    ForEach(games, id: \.name) { game in
+                        NavigationLink(value: game) {
+                            Text(game.name)
                         }
                     }
                 }
@@ -33,8 +47,11 @@ struct ContentView: View {
                         .font(.largeTitle).bold()
                 }
             }
+            .navigationDestination(for: Game.self) { game in
+                Text("\(game.name) - \(game.rating)")
+                    .font(.largeTitle.bold())
+            }
         }
-        
     }
 }
 
@@ -48,4 +65,9 @@ struct Platform: Hashable {
     let name: String
     let imageName: String
     let color: Color
+}
+
+struct Game: Hashable {
+    let name: String
+    let rating: String
 }
