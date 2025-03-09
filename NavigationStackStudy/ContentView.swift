3bca278052eaca6080a373d16a60fd9c74ff638a
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-03-09 12:36:38 +0200

    create Navigation Stack Example

--- NavigationStackStudy/ContentView.swift
+++ NavigationStackStudy/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var platforms: [Platform] = [ .init(name: "Xbox", imageName: "xbox.logo", color: .green),
+                                  .init(name: "PlayStation", imageName: "playstation.logo", color: .indigo),
+                                  .init(name: "PC", imageName: "pc", color: .pink),
+                                  .init(name: "Mobile", imageName: "iphone", color: .mint)]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List{
+                Section("Platforms") {
+                    ForEach(platforms, id: \.name) { platform in
+                        NavigationLink(value: platform) {
+                            Label(platform.name, systemImage: platform.imageName)
+                                .foregroundStyle(platform.color)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Gaming")
+            .navigationDestination(for: Platform.self) { platform in
+                ZStack {
+                    platform.color.ignoresSafeArea()
+                    Label(platform.name, systemImage: platform.imageName)
+                        .font(.largeTitle).bold()
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+
+struct Platform: Hashable {
+    let name: String
+    let imageName: String
+    let color: Color
 }
