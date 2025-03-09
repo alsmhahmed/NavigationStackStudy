c74d005020ff8226319414320a08b0545755276b
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-03-09 14:06:22 +0200

    test

---
+++ NavigationStackStudy/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  NavigationStackStudy
+//
+//  Created by alsmh ahmed on 09/03/2025.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
