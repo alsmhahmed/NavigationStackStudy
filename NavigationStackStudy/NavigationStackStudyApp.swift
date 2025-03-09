c969a42421e046f08226c05fa41ec3f4d84e5c9c
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-03-09 11:49:45 +0200

    Initial Commit

---
+++ NavigationStackStudy/NavigationStackStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackStudyApp.swift
+//  NavigationStackStudy
+//
+//  Created by alsmh ahmed on 09/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
